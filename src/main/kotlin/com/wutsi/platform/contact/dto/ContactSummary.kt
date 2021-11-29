23a4373eaafae718b71e1eb9d027482474b08136
wutsi <herve.tchepannou@wutsi.com>
2021-11-29 09:59:07 -0500

    update api

--- src/main/kotlin/com/wutsi/platform/contact/dto/ContactSummary.kt
+++ src/main/kotlin/com/wutsi/platform/contact/dto/ContactSummary.kt
@@ -3,7 +3,7 @@ package com.wutsi.platform.contact.dto
 import java.time.OffsetDateTime
 import kotlin.Long
 
-public data class Contact(
+public data class ContactSummary(
   public val accountId: Long = 0,
   public val contactId: Long = 0,
   public val created: OffsetDateTime = OffsetDateTime.now()
