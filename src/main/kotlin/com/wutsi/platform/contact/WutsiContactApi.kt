d7097df8b02de84f64a11288dfc801bb37936eb6
wutsi <herve.tchepannou@wutsi.com>
2021-11-30 09:56:27 -0500

    add sync endpoint

--- src/main/kotlin/com/wutsi/platform/contact/WutsiContactApi.kt
+++ src/main/kotlin/com/wutsi/platform/contact/WutsiContactApi.kt
@@ -2,6 +2,8 @@ package com.wutsi.platform.contact
 
 import com.wutsi.platform.contact.dto.SearchContactRequest
 import com.wutsi.platform.contact.dto.SearchContactResponse
+import com.wutsi.platform.contact.dto.SyncContactRequest
+import com.wutsi.platform.contact.dto.SyncContactResponse
 import feign.Headers
 import feign.RequestLine
 
@@ -9,4 +11,8 @@ public interface WutsiContactApi {
   @RequestLine("POST /v1/contacts/search")
   @Headers(value=["Content-Type: application/json"])
   public fun searchContact(request: SearchContactRequest): SearchContactResponse
+
+  @RequestLine("POST /v1/contacts/sync")
+  @Headers(value=["Content-Type: application/json"])
+  public fun syncContacts(request: SyncContactRequest): SyncContactResponse
 }
