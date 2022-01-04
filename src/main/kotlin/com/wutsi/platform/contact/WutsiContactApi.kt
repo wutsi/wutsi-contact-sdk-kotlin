32601c2b4828b6db0da96c844e179a38c34418e5
wutsi <herve.tchepannou@wutsi.com>
2022-01-04 13:10:55 -0500

    update api

--- src/main/kotlin/com/wutsi/platform/contact/WutsiContactApi.kt
+++ src/main/kotlin/com/wutsi/platform/contact/WutsiContactApi.kt
@@ -1,5 +1,7 @@
 package com.wutsi.platform.contact
 
+import com.wutsi.platform.contact.dto.CreateContactRequest
+import com.wutsi.platform.contact.dto.CreateContactResponse
 import com.wutsi.platform.contact.dto.SearchContactRequest
 import com.wutsi.platform.contact.dto.SearchContactResponse
 import com.wutsi.platform.contact.dto.SyncContactRequest
@@ -8,6 +10,10 @@ import feign.Headers
 import feign.RequestLine
 
 public interface WutsiContactApi {
+  @RequestLine("POST /v1/contacts")
+  @Headers(value=["Content-Type: application/json"])
+  public fun createContact(request: CreateContactRequest): CreateContactResponse
+
   @RequestLine("POST /v1/contacts/search")
   @Headers(value=["Content-Type: application/json"])
   public fun searchContact(request: SearchContactRequest): SearchContactResponse
